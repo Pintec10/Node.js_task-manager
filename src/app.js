404f588eb5b723944e9a689bf425e63dc3f8f51c
Roberto Milani <milani@strands.com>
2021-05-04 17:02:23 +0200

    cleaned and commented CORS code

--- src/app.js
+++ src/app.js
@@ -8,9 +8,9 @@ const app = express();
 
 app.use(express.json());    //auto parses incoming json to an object in req.body
 
-// tentative to fix CORS issue
+// prevent CORS issues
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', '*');     //tighten up allowed origin for production
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
